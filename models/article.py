7c1bf228f179d8e5f634056ddfe807be2f197e63
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-12-15 17:20:22 +0300

    Complete Challenge

--- models/article.py
+++ models/article.py
@@ -1,10 +1,39 @@
+from database.connection import get_db_connection
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+        self._id = id
+        self._title = title
+        self._content = content
+        self._author_id = author_id
+        self._magazine_id = magazine_id
+        self.create_in_db()
 
-    def __repr__(self):
-        return f'<Article {self.title}>'
+    def create_in_db(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT OR IGNORE INTO articles (id, title, content, author_id, magazine_id) VALUES (?, ?, ?, ?, ?)",
+            (self._id, self._title, self._content, self._author_id, self._magazine_id)
+        )
+        conn.commit()
+        conn.close()
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def author_id(self):
+        return self._author_id
+
+    @property
+    def magazine_id(self):
+        return self._magazine_id
+
+    def __str__(self):
+        return f"Article(id={self._id}, title={self._title}, content={self._content}, author_id={self._author_id}, magazine_id={self._magazine_id})"
--- models/author.py
+++ models/author.py
@@ -1,7 +1,49 @@
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
-        self.id = id
-        self.name = name
+        self._id = id
+        self._name = name
+        self.create_in_db()
 
-    def __repr__(self):
-        return f'<Author {self.name}>'
+    def create_in_db(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT OR IGNORE INTO authors (id, name) VALUES (?, ?)", (self._id, self._name))
+        conn.commit()
+        conn.close()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    def articles(self):
+        from models.article import Article  
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self._id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Article(row[0], row[1], row[2], self, row[4]) for row in rows]
+
+    def magazines(self):
+        from models.magazine import Magazine  
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = """
+            SELECT DISTINCT magazines.* 
+            FROM magazines 
+            JOIN articles ON magazines.id = articles.magazine_id 
+            WHERE articles.author_id = ?
+        """
+        cursor.execute(query, (self._id,))
+        rows = cursor.fetchall()
+        conn.close()
+        return [Magazine(row[0], row[1], row[2]) for row in rows]
+
+    def __str__(self):
+        return f"Author: {self._name}, ID: {self._id}"
